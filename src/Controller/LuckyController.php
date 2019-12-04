c9419e03d257db70885d0fa0ba38c197bacf3b89
knows1 <info@knows1.com>
2019-12-04 09:13:38 +0100

    +

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,19 +1,14 @@
-
-
-
 <?php
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class LuckyController
 {
-
-  /**
+    /**
      * @Route("/lucky/number")
-     */
+   */  
     public function number()
     {
         $number = random_int(0, 100);
