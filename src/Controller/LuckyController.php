89ebedf626815ee5eb3a74d368574553442b2beb
knows1 <info@knows1.com>
2019-12-04 09:08:20 +0100

    in /home/knows1/my_project_name/src/Controller/LuckyController.php (line 22)

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,29 +1,5 @@
 
 
-/* Previously
-<?php
-// src/Controller/LuckyController.php
-namespace App\Controller;
-
-use Symfony\Component\HttpFoundation\Response;
-
-class LuckyController
-{
-    public function number()
-    {
-        $number = random_int(0, 100);
-
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
-    }
-}
-
-*/
-/*
- composer require annotations
- Annotation Routes
-*/
 
 <?php
 // src/Controller/LuckyController.php
