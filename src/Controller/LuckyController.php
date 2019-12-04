b61ade638f05ebb3da615bcb4e115db3d06fe357
knows1 <info@knows1.com>
2019-12-04 09:00:45 +0100

    Annotation Routes

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,3 +1,6 @@
+
+
+/* Previously
 <?php
 // src/Controller/LuckyController.php
 namespace App\Controller;
@@ -6,6 +9,35 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController
 {
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
+
+*/
+/*
+ composer require annotations
+ Annotation Routes
+*/
+
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+
+  /**
+     * @Route("/lucky/number")
+     */
     public function number()
     {
         $number = random_int(0, 100);
